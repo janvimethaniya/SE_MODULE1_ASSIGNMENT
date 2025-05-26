e2e2f4f3cb87b2e9f2b13eeb4d7214847f60b1cd
janvimethaniya <methaniyajanvi636@gmail.com>
2025-05-26 10:15:24 +0530

    SE_MODULE1_ASSIGNMENT
    SE_MODULE1_ASSIGNMENT DONE

---
+++ ASSET1/PRACTICAL/hello.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>Hello World in JavaScript</title>
+</head>
+<body>
+  <h1>My First JavaScript Program</h1>
+<script>
+    console.log("Hello, World!");
+    
+  </script>
+</body>
+</html>
